0ecabf25c0cc9e71e9c8a8de5bf87c711f59404d
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-04-29 09:52:42 -0300

    fix: customer factory

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,10 +1,8 @@
 FactoryBot.define do
   factory :customer do
-    fake_phone_number = Faker::PhoneNumber.cell_phone
-
     name { Faker::Name.unique.name }
     email { Faker::Internet.email }
-    whatsapp { fake_phone_number }
-    phone_number { fake_phone_number }
+    whatsapp { Faker::PhoneNumber.cell_phone }
+    phone_number { Faker::PhoneNumber.cell_phone }
   end
 end
