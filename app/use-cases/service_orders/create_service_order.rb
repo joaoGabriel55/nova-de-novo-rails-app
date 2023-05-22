97f45220307f98e6521c9f306030a55ffb3e83f9
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-05-22 18:56:51 -0300

    refactor: CreateServiceOrder use-case

--- app/use-cases/service_orders/create_service_order.rb
+++ app/use-cases/service_orders/create_service_order.rb
@@ -7,23 +7,23 @@ module ServiceOrders
     end
 
     def call
-      validate_service_order!(service_order)
-
-      service_order.save!
+      service_order.save! unless validate_service_order!
     rescue ActiveRecord::RecordInvalid
       raise ServiceOrders::CreateServiceOrderError
     end
 
     private
 
-    def validate_service_order!(service_order)
-      dressmaker = Dressmakers::FindDressmaker.new(id: service_order.dressmaker.id).call
-
+    def validate_service_order!
       if service_order.tasks.empty?
         raise ServiceOrders::EmptyServiceOrderTasksError
-      elsif dressmaker.service_orders.count + 1 > dressmaker.max_service_quantity
+      elsif dressmaker.service_orders.length + 1 > dressmaker.max_service_quantity
         raise ServiceOrders::ExceededMaxNumberOfDressmakerOrderServicesError
       end
+    end
+
+    def dressmaker
+      @dressmaker ||= Dressmakers::FindDressmaker.new(id: service_order.dressmaker.id).call
     end
 
     attr_accessor :service_order
