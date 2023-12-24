6da4601b8d7fc94e7b6b907f9dfe878a2f20bc77
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-12-24 08:10:12 -0300

    build: fix vite

--- config/application.rb
+++ config/application.rb
@@ -43,5 +43,6 @@ module NovaDeNovo
     config.time_zone = 'Brasilia'
 
     config.autoload_paths += %W[#{config.root}/app, #{config.root}/lib]
+    config.assets.js_compressor = :uglifier
   end
 end
