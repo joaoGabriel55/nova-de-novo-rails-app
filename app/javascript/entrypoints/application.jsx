82ad78c05f0e70fcff5d9b3cb6b6adc65221938c
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-12-26 17:12:57 -0300

    chore: minor update

--- app/javascript/entrypoints/application.jsx
+++ app/javascript/entrypoints/application.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { CustomersAutocomplete } from "./customers_autocomplete";
 import render from "./react_render";
+import "flowbite";
+import "flowbite-datepicker";
 
 render("autocomplete", <CustomersAutocomplete />);
--- app/javascript/entrypoints/react_render.js
+++ app/javascript/entrypoints/react_render.js
@@ -1,6 +1,9 @@
 import { createRoot } from "react-dom/client";
 
 export default function render(id, component) {
-  const root = createRoot(document.getElementById(id));
-  root.render(component);
+  const element = document.getElementById(id);
+  if (element) {
+    const root = createRoot(document.getElementById(id));
+    root.render(component);
+  }
 }
