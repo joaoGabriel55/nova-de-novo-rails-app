b0b0410d77412b841ce042a03fcafcc103b1bc8b
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2023-12-28 09:28:07 -0300

    chore: remove vite references

--- app/javascript/entrypoints/react_render.js
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-
-export default function render(id, component) {
-  const element = document.getElementById(id);
-  if (element) {
-    const root = createRoot(document.getElementById(id));
-    root.render(component);
-  }
-}
